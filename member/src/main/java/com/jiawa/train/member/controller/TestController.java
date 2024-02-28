67b303c27d8f7da75f0c59a2e329217c178b5b31
zhaoci1 <1723294884@qq.com>
2024-02-28 14:33:43 +0800

    新增member会员模块

--- member/src/main/java/com/jiawa/train/member/controller/TestController.java
+++ member/src/main/java/com/jiawa/train/member/controller/TestController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello";
+        return "Hello123";
     }
 }
