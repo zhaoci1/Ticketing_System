857dbbe15d0211839ccdbb1cc1b2154db766b258
zhaoci1 <1723294884@qq.com>
2024-03-01 14:57:34 +0800

    4.14 集成mybatis代码生成器

--- member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
+++ member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
@@ -1,6 +1,7 @@
 package com.jiawa.train.member.config;
 
 import org.apache.juli.logging.Log;
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -10,6 +11,7 @@ import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.jiawa")
+@MapperScan("com.jiawa.train.member.mapper")
 public class MemberApplication {
     private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
 
---
+++ member/src/main/java/com/jiawa/train/member/service/MemberService.java
@@ -0,0 +1,15 @@
+package com.jiawa.train.member.service;
+
+import com.jiawa.train.member.mapper.MemberMapper;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+    @Resource
+    private MemberMapper memberMapper;
+
+    public int count() {
+        return Math.toIntExact(memberMapper.countByExample(null));
+    }
+}
