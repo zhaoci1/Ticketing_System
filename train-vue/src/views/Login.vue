24d098f4b627268bf13da9fc824b5aa0574c4514
zhaoci1 <1723294884@qq.com>
2024-03-04 16:41:45 +0800

    5.9 集成axios，发起后端请求有跨域问题

--- train-vue/src/views/Login.vue
+++ train-vue/src/views/Login.vue
@@ -1,46 +1,49 @@
 <template>
   <div>
     <a-row class="login">
-    <a-col :span="8" :offset="8" class="login-main">
-      <h1 style="text-align: center"><rocket-two-tone />&nbsp;甲蛙12306售票系统</h1>
-      <a-form
+      <a-col :span="8" :offset="8" class="login-main">
+        <h1 style="text-align: center">
+          <rocket-two-tone />&nbsp;甲蛙12306售票系统
+        </h1>
+        <a-form
           :model="loginForm"
           name="basic"
           autocomplete="off"
           @finish="onFinish"
           @finishFailed="onFinishFailed"
-      >
-        <a-form-item
+        >
+          <a-form-item
             label=""
             name="mobile"
             :rules="[{ required: true, message: '请输入手机号!' }]"
-        >
-          <a-input v-model:value="loginForm.mobile" placeholder="手机号"/>
-        </a-form-item>
+          >
+            <a-input v-model:value="loginForm.mobile" placeholder="手机号" />
+          </a-form-item>
 
-        <a-form-item
+          <a-form-item
             label=""
             name="code"
             :rules="[{ required: true, message: '请输入验证码!' }]"
-        >
-          <a-input v-model:value="loginForm.code">
-            <template #addonAfter>
-              <a @click="sendCode">获取验证码</a>
-            </template>
-          </a-input>
-          <!--<a-input v-model:value="loginForm.code" placeholder="验证码"/>-->
-        </a-form-item>
+          >
+            <a-input v-model:value="loginForm.code">
+              <template #addonAfter>
+                <a @click="sendCode">获取验证码</a>
+              </template>
+            </a-input>
+            <!--<a-input v-model:value="loginForm.code" placeholder="验证码"/>-->
+          </a-form-item>
 
-        <a-form-item>
-          <a-button type="primary" block @click="login">登录</a-button>
-        </a-form-item>
-      </a-form>
-    </a-col>
-  </a-row>
+          <a-form-item>
+            <a-button type="primary" block @click="login">登录</a-button>
+          </a-form-item>
+        </a-form>
+      </a-col>
+    </a-row>
   </div>
 </template>
 <script>
 import { defineComponent, reactive } from "vue";
+import Axios from "@/api/loginApi";
 export default defineComponent({
   name: "login",
   setup() {
@@ -54,10 +57,16 @@ export default defineComponent({
     const onFinishFailed = (errorInfo) => {
       console.log("Failed:", errorInfo);
     };
+    const login = () => {
+      Axios.login(loginForm).then(res=>{
+        console.log(res);
+      })
+    };
     return {
       loginForm,
       onFinish,
       onFinishFailed,
+      login,
     };
   },
 });
