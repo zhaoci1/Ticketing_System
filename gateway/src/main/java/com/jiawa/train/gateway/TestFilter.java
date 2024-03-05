aad32398ea686228cf98a8805342f1e46d27065d
zhaoci1 <1723294884@qq.com>
2024-03-05 22:01:57 +0800

    6.7 演示gateway过滤器的使用

---
+++ gateway/src/main/java/com/jiawa/train/gateway/TestFilter.java
@@ -0,0 +1,21 @@
+package com.jiawa.train.gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class TestFilter implements GlobalFilter {
+
+    private static final Logger Log = LoggerFactory.getLogger(TestFilter.class);
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        System.out.println(1);
+        Log.info("TestFilter");
+        return chain.filter(exchange);
+    }
+}
