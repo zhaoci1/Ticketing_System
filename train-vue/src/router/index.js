c84d0783f7488a9a004311e44a102df2fa69d283
zhaoci1 <1723294884@qq.com>
2024-03-10 10:48:29 +0800

    7.9，增加页面跳转效果

--- train-vue/src/router/index.js
+++ train-vue/src/router/index.js
@@ -1,14 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import store from "@/store";
-import { notification } from "ant-design-vue";
+import { notification } from  "ant-design-vue";
 
 const routes = [
-  {
-    path: "/login",
-    name: "login",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Login.vue"),
-  },
   {
     path: "/",
     component: () =>
@@ -22,17 +16,27 @@ const routes = [
         path:"welcome",
         component: () =>import( "../views/main/Welcome.vue"),
     },
+    {
+      path:"passenger",
+      component: () =>import( "../views/main/Passenger.vue"),
+    },
     ]
   },
   {
-    path: "",
-    redirect: '/welcome'
+    path: "/login",
+    name: "login",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Login.vue"),
   },
   {
     path: "/main",
     name: "main",
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/Main.vue"),
+  },
+  {
+    path: "",
+    redirect: '/welcome'
   },
 ];
 
@@ -44,12 +48,11 @@ router.beforeEach((to, from, next) => {
   if (
     // 对自定义变量进行校验，login因为有变量，所以返回值为true，其他没有这个变量的，默认都是false，也就是说都会被拦截
     to.matched.some(function (item) {
-      console.log(item, "是否需要登录校验：", item.meta.loginRequire || false);
+      // console.log(item, "是否需要登录校验：", item.meta.loginRequire || false);
       return item.meta.loginRequire;
     })
   ) {
     const member = store.state.member;
-    console.log("页面登录校验开始：", member);
     // token不存在则表示未登录或者超时
     if (!member.token) {
       console.log("用户未登录或者登录超时！");
