dc08feddbc05b41c993c3574edba9406ed1da12c
zhaoci1 <1723294884@qq.com>
2024-03-16 15:41:15 +0800

    9.11 修改了返回结果总数错误的BUG

---
+++ admin/src/components/the-select.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <a-select
+      v-model:value="trainCode"
+      show-search
+      allowClear
+      :filterOption="filterTrainCodeOption"
+      @change="onChange"
+      placeholder="请选择车次"
+    >
+      <a-select-option
+        v-for="item in trainAll"
+        :key="item.code"
+        :value="item.code"
+        :label="item.code + item.start + item.end"
+      >
+        {{ item.code }} | {{ item.start }} ~ {{ item.end }}
+      </a-select-option>
+    </a-select>
+  </div>
+</template>
+
+<script>
+import { defineComponent, onMounted, ref, watch } from "vue";
+import Axios from "@/api/trainStationApi";
+
+export default defineComponent({
+  // 父子组件传值
+  props: ["modelValue"],
+  //   定义一个事件change
+  emits: ["update:modelValue", "change"],
+  setup(props, {emit}) {
+    let trainAll = ref([]);
+    let trainCode = ref();
+    const filterTrainCodeOption = (input, option) => {
+      return option.label.toLowerCase().indexOf(input.toLowerCase()) >= 0;
+    };
+    const queryTrainAll = () => {
+      Axios.queryAll().then((res) => {
+        trainAll.value = res.data;
+        console.log(res);
+      });
+    };
+    /**
+     * 将选中的车次信息返回给父组件
+     */
+    const onChange = (value) => {
+      emit("update:modelValue", value);
+      let train = trainAll.value.filter((item) => item.code === value)[0];
+      if (train == null) {
+        train = {};
+      }
+      emit("change", train);
+    };
+    onMounted(() => {
+      queryTrainAll();
+    });
+    // 监控父组件的值
+    watch(
+      () => props.modelValue,
+      () => {
+        trainCode.value = props.modelValue;
+      },
+      { immediate: true }
+    );
+    return {
+      trainAll,
+      queryTrainAll,
+      filterTrainCodeOption,
+      onChange,
+    };
+  },
+});
+</script>
