84931a0f6b67b191c879eda42ed78b0c975fbdb9
zhaoci1 <1723294884@qq.com>
2024-03-17 21:39:41 +0800

    10.3 禁止任务并发执行

--- batch/src/main/java/com/jiawa/train/batch/job/TestJob.java
+++ batch/src/main/java/com/jiawa/train/batch/job/TestJob.java
@@ -1,9 +1,11 @@
 package com.jiawa.train.batch.job;
 
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+@DisallowConcurrentExecution
 public class TestJob implements Job {
 
     @Override
