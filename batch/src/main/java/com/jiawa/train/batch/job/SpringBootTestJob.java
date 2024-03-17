02b78ee1ead94a971dc7ffea3e6b830a330f3faa
zhaoci1 <1723294884@qq.com>
2024-03-17 21:27:15 +0800

    10.2 集成quartz，和spring boot自带的定时任务，可以共存

---
+++ batch/src/main/java/com/jiawa/train/batch/job/SpringBootTestJob.java
@@ -0,0 +1,15 @@
+//package com.jiawa.train.batch.job;
+//
+//import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//@EnableScheduling
+//@Component
+//public class SpringBootTestJob {
+//
+//    @Scheduled(cron = "0/5 * * * * ?")
+//    private void test() {
+//        System.out.println("SpringBootTestJob Test");
+//    }
+//}
